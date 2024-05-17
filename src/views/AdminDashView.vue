a5309faae935ec5b6cbe14de44f68876af10e7df
Bela von Wangenheim <belavw@gmx.de>
2024-05-17 10:05:09 +0200

    New AdminDashView

---
+++ src/views/AdminDashView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container mt-5">
+    <h2>Admin-Dashboard</h2>
+    <p>Admin specific controls and information will be displayed here.</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AdminDashView',
+});
+</script>
+
+<style scoped>
+.container {
+  max-width: 800px;
+}
+</style>
+
+<style scoped>
+.container {
+  max-width: 500px;
+}
+</style>
