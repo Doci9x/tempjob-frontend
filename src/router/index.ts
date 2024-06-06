1b80de3382ae2f4817079b49434af03c80cbf217
Bela von Wangenheim <belavw@gmx.de>
2024-06-06 04:25:10 +0200

    fehlerbehebung

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,50 +1,48 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import WelcomeView from '@/pages/WelcomeView.vue';
+
 import TempjobView from '@/pages/TempjobView.vue';
 import RegisterView from '@/pages/RegisterView.vue';
 import LoginView from '@/pages/LoginView.vue';
 import JobCreate from '@/pages/JobCreate.vue';
 import JobDetails from '@/pages/JobDetails.vue';
+import WelcomeView from '@/pages/WelcomeView.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Welcome',
+    component: WelcomeView
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: RegisterView
+  },
+  {
+    path: '/tempjob',
+    name: 'TempJob',
+    component: TempjobView
+  },
+  {
+    path: '/job-create',
+    name: 'JobCreate',
+    component: JobCreate
+  },
+  {
+    path: '/job/:id',
+    name: 'JobDetails',
+    component: JobDetails
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  linkActiveClass: 'active',
-  routes: [
-    {
-      path: '/',
-      redirect: '/welcome'
-    },
-    {
-      path: '/welcome',
-      name: 'Welcome',
-      component: WelcomeView
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: LoginView
-    },
-    {
-      path: '/register',
-      name: 'Register',
-      component: RegisterView
-    },
-    {
-      path: '/tempjob',
-      name: 'Tempjob',
-      component: TempjobView
-    },
-    {
-      path: '/job-create',
-      name: 'JobCreate',
-      component: JobCreate
-    },
-    {
-      path: '/job/:id',
-      name: 'JobDetails',
-      component: JobDetails
-    }
-  ]
+  routes
 });
 
 export default router;
