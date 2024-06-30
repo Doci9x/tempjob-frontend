27c38da15586d9c8eabad3e4b5ffc117b2d22743
Bela von Wangenheim <belavw@gmx.de>
2024-06-30 20:35:10 +0200

    New jest config + setup

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.js$': 'babel-jest'
+  },
+  testMatch: ['**/tests/**/*.spec.js'],
+  setupFiles: ['./jest.setup.js']
+};
